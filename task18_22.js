384db51dc2442fc642d3dfba5935049e70debd82
Reham236 <reham.salah2362@gmail.com>
2025-11-26 20:56:06 +0200

    Task 18_22 (Operators)

---
+++ task18_22.js
@@ -0,0 +1,52 @@
+// Task 1
+// Replace ? With Arithmetic Operators
+// console.log(10 ? 20 ? 15 ? 3 ? 190 ? 10 ? 400); // 0
+
+// Task 2
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num*num - num); // 6
+
+// Soultion Three
+console.log(num*num / num +num); // 6
+
+// Soultion Four
+console.log(num* num -num); // 6 
+
+// Solution Five
+console.log("Write Your Code Here"); // 6
+
+// Solution Six
+console.log("Write Your Code Here"); // 6
+  
+// Task 3
+let Num = "10";
+
+// Solution One
+console.log(+Num + +Num); // 20
+
+// Solution Two
+console.log(+Num* +Num / +Num + +Num); // 20
+
+// Solution Three
+console.log(+Num + +Num + +Num - +Num); // 20
+
+// Solution Four
+console.log("Write Your Code Here"); // 20
+    
+
+// Task 4
+let points = 10;
+
+// Write Your Code Here
+points += 3;
+
+console.log(points); // 13
+
+// Write Your Code Here
+points -= 5;
+console.log(points); // 8;
